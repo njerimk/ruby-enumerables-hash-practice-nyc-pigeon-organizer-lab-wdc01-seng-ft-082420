031c77c13ebe8b34477f4fa174f949c7ac4d90bf
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-28 22:27:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,7 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  name_array = data[:gender][:female] + data[:gender][:male]
-  new_list = Hash[name_array.collect { |item| [item, {}] } ]
-    data.each do |key, value|
-      if key == new_list[key]
-        new_list.item[key] = key
+  
     end
   end
   puts new_list
