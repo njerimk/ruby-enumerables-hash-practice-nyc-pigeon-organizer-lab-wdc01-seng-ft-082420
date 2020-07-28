ee31399e9f6f036eba5e0cf786a4a0c0cdad187e
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-28 22:32:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,19 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  final_results
+final_results= data.each_with_object({}) do |(key, value), final_aray|
+  value.each do |inner_key, names|
+    names.each do |name|
+      if !final_array[name]
+        final_array[name] = {}
+      end
+      if !final_array[name][key]
+        !final_array[name][key] = []
+      end
+      final_arrary[name][key].push(inner_key.to_s)
+      end
     end
   end
-  puts new_list
 end
 
 pigeon_data = {
